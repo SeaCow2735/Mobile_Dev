f2a2f86f0aabe190c670a87346107b47ad4b5141
Nguyễn Phạm Hùng Nhân <thisisaseacow@gmail.com>
2025-11-28 17:32:19 +0700

    add code

--- LazyList/app/src/main/java/com/example/lazylist/MainActivity.kt
+++ LazyList/app/src/main/java/com/example/lazylist/MainActivity.kt
@@ -44,64 +44,82 @@ fun LazyDemoScreen() {
         mutableStateListOf("1", "2", "3", "4", "5","6","7","8","9","10","11","12","13")
     }
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                columnList.add("Item ${columnList.size + 1}")
-                rowList.add("${columnList.size + 1}")
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
-    ) { inner ->
-        Column(
+        Scaffold(
             modifier = Modifier
-                .padding(inner)
-                .padding(16.dp)
-        ) {
-            // LazyRow
-            Text("LazyRow Demo:", style = MaterialTheme.typography.titleMedium)
-            Spacer(Modifier.height(8.dp))
-            LazyRow {
-                items(rowList) { index ->
-                    Text(
-                        text = "$index",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(12.dp)
-                            .clickable {
-                                Toast.makeText(context, "Chọn: $index", Toast.LENGTH_SHORT).show()
-                            }
-                            .background(Color(0xFF363636)),
-                        color = Color.White
-                    )
+                .fillMaxSize()
+                .background(Color.Black),
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                    columnList.add("Item ${columnList.size + 1}")
+                    rowList.add("${columnList.size + 1}")
+                }) {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
                 }
             }
-
-            Spacer(Modifier.height(20.dp))
-
-            // LazyColumn
-            Text("LazyColumn Demo:", style = MaterialTheme.typography.titleMedium)
-            Spacer(Modifier.height(8.dp))
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) { inner ->
+            Column(
+                modifier = Modifier
+                    .padding(inner)
+                    .padding(16.dp)
             ) {
-                items(columnList) { index ->
-                    Text(
-                        text = "$index",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(12.dp)
-                            .clickable {
-                                Toast.makeText(context, "Chọn: $index", Toast.LENGTH_SHORT).show()
-                            }
-                            .background(Color(0xFF363636)),
-                        color = Color.White
-                    )
+                // LazyRow
+                Text("LazyRow Demo:", style = MaterialTheme.typography.titleMedium)
+                Spacer(Modifier.height(8.dp))
+                LazyRow {
+                    items(rowList) { index ->
+                        Box (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp)
+                                .background(Color(0xFF363636))
+                        ){
+                            Text(
+                                text = "$index",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(12.dp)
+                                    .clickable {
+                                        Toast.makeText(context, "Chọn: $index", Toast.LENGTH_SHORT).show()
+                                    }
+                                    .background(Color(0xFF363636)),
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+
+                Spacer(Modifier.height(20.dp))
+
+                // LazyColumn
+                Text("LazyColumn Demo:", style = MaterialTheme.typography.titleMedium)
+                Spacer(Modifier.height(8.dp))
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(columnList) { index ->
+                        Box (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp)
+                                .background(Color(0xFF363636))
+                        ){
+                            Text(
+                                text = "$index",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(12.dp)
+                                    .clickable {
+                                        Toast.makeText(context, "Chọn: $index", Toast.LENGTH_SHORT).show()
+                                    }
+                                    .background(Color(0xFF363636)),
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
+
